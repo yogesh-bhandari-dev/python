71f70890b78727de60aa730b64d2d29b8b05a871
yogesh-bhandari-dev <yogeshbhandari.dev@gmail.com>
2026-03-18 14:46:55 +0530

    implement basics of python

---
+++ basics/basics.py
@@ -0,0 +1,20 @@
+a = "hello"
+print (a)
+
+#variable declaration i.e holds data of a container
+#
+
+#
+#swapping of two variables by temporary variables
+a = 80
+b= 50
+
+temp = a
+print (temp)
+
+a=b
+print (a)
+b=temp
+print(b)
+print("value of a is ",a)
+print("value of b is ",b)
---
+++ basics/conditionls.py
@@ -0,0 +1,6 @@
+a = int(input("Enter your age: "))
+
+if a >= 18:
+    print("you can vote")
+else:
+    print("you cannot vote")
